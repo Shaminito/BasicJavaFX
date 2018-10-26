144c019866e2d2739c95461544ed470afed3bc7a
shaminito <shaminito@gmail.com>
2018-10-26 12:04:15 +0200

    ...

--- BasicJavaFX/src/application/Main.java
+++ BasicJavaFX/src/application/Main.java
@@ -6,14 +6,13 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	@FXML
-	private ImageView forestview;
 	private AnchorPane mypane;
 
 	// modifica el método start de la clase principal
@@ -22,10 +21,10 @@ public class Main extends Application {
 
 		// primero cargamos la vista fxml en el FXMLLoader
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(
-				"forest.fxml"));
+				"../forest.fxml"));
 
 		// y opcionalmente un controlador para inicializar algún componente
-		loader.setController(new Controller());
+		loader.setController(new SetBlur());
 
 		// ahora sobre el panel mypane cargamos la vista que dejamos en el
 		// FXMLLoader
@@ -37,8 +36,24 @@ public class Main extends Application {
 		stage.show();
 	}
 
+	// la clase controladora que añade un efecto a la imagen
+	class SetBlur {
+
+		// la etiqueta FXML permite acceder al componente imageView ya creado en
+		// fxml
+		@FXML
+		private ImageView forestview;
+
+		@FXML
+		public void initialize() {
+			forestview.setEffect(new GaussianBlur(2));
+		}
+
+	}
+
 	// arranca la aplicación
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
